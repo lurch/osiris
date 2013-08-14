e6b1342df6a4e9c7bbbfaf01cb4912b19a814471
Kevin Mihelich <kevin@archlinuxarm.org>
2013-08-14 15:20:13 -0600

    Update partition_setup.sh

--- partition_setup.sh
+++ partition_setup.sh
@@ -3,18 +3,9 @@
 # NOOBS partition setup script for Arch Linux ARM
 #  - part1 == boot partition (FAT), part2 == root partitions (ext4)
 #  - example usage:
-#    ./partition_setup.sh part1=/dev/mmcblk0p7 id1=LABEL=root part2=/dev/mmcblk0p8 id2=UUID=550e8400-e29b-41d4-a716-446655440000
+#    part1=/dev/mmcblk0p7 part2=/dev/mmcblk0p8 ./partition_setup.sh
 
 # extract and set part1 and part2 variables
-part1=
-part2=
-while [[ $# > 0 ]]; do
-  part=`expr "$1" : '\(^part[1-2]\)'`
-  if [[ "${part}" != '' ]]; then
-    eval ${part}=`expr "$1" : '.*=\(.*\)'`
-  fi
-  shift
-done
 
 if [[ ${part1} == '' || ${part2} == '' ]]; then
   echo "error: part1 and part2 not specified"
